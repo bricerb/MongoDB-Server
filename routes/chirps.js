91c1055bbc052432b2f60a4c541c6bb67682932f
Richard Blanch <Brice@Brices-Stash.local>
2017-11-03 17:33:09 -0400

    added restful endpoints

--- routes/chirps.js
+++ routes/chirps.js
@@ -5,32 +5,11 @@ const router = express.Router();
 // Chirp Repo
 const chirps = require('../models/Chirps');
 
-// Chirps Endpoints
+// MVC Endpoints
 router.get('/', (req,res) => {
     chirps.find({})
     .then((data) => {
         res.render('chirps', {chirps: data});
-    })
-});
-
-router.get('/all', (req,res) => {
-    chirps.find({})
-    .then((data) => {
-        res.send({'data', data});
-    })
-})
-
-router.post('/new', (req,res) => {
-    let newPost = {
-        title: req.body.title,
-        post: req.body.post,
-        comment: req.body.comment,
-        author: req.body.author,
-    }
-
-    chirps.create(newPost)
-    .then((data) => {
-        res.send('Success');
     })
 });
 
@@ -55,5 +34,49 @@ router.get("/delete/all", (req,res) => {
         res.redirect('/chirps');
     });
 });
+
+// RESTful endpoints
+
+router.get('/all', (req,res) => {
+    chirps.find({})
+    .then((data) => {
+        res.send({'data', data});
+    })
+})
+
+router.post('/new', (req,res) => {
+    let newPost = {
+        title: req.body.title,
+        post: req.body.post,
+        comment: req.body.comment,
+        author: req.body.author,
+    }
+
+    chirps.create(newPost)
+    .then((data) => {
+        res.send({status: 'Successfully created'});
+    })
+});
+
+router.put('/update/:id', (req,res) => {
+    let obj = req.body;
+    chirps.update({_id: req.params.id}, req.body)
+    .then((data) => {
+        var json = {
+            data: data
+        }
+        res.send(json);
+    })
+});
+
+router.delete('/delete/:id', (req,res) => {
+    let id = req.params.id;
+    chirps.deleteOne({'_id': id})
+    .then(() => {
+        res.send({status: 'Successfully deleted'});
+    })
+});
+
+
 
 module.exports = router;
